d5dfc86b9d9fee0792dcb937ccb64f879378f10e
Tommy Unger <tunger10@gmail.com>
2016-04-30 02:36:48 -0400

    Progress on bit manipulations. Not finished.

--- discos/mymem.c
+++ discos/mymem.c
@@ -1,51 +1,8 @@
 #include "discos.h"
 #include <stdio.h>
-#include <stdlib.h>
 
-int rd_creat(char *pathname){
-  return 0;
-} 
-
-int rd_mkdir(char *pathname){
-return 0;
-} 
-
-int rd_close(int fd){
-return 0;
-} 
-
-int rd_read(int fd, char *address, int num_bytes){
-return 0;
-}
-
-int rd_write(int fd, char *address, int num_bytes){
-return 0;
-}
-
-int rd_lseek(int fd, int offset){
-return 0;
-}
-
-int rd_unlink(char *pathname){
-return 0;
-}
-
-int rd_readdir(int fd, char *address){
-return 0;
-} 
-
-/*
-int scan_bitmap(unsigned char *ch, int len){
-  int i, bit;
-  
-  for(i=0; i<len; i++){
-    if(ch != 
-  }
-	
-  return -1;
-}
-*/
-int scan_byte(unsigned char ch){
+int next_zero_byte(unsigned char ch){
+  //Find index of next unset bit in byte.
   int i;
   for(i=0; i<8; i++) {
     if((ch & (unsigned char) 1) == 0) {
@@ -53,34 +10,63 @@ int scan_byte(unsigned char ch){
     }
     ch = ch >> 1;
   }
+  return -1;
+}
 
+int next_zero_bitmap(unsigned char *ch, int len){
+  //Find index of next unset bit in bitmap.
+  int i, bit;
+  bit = 0;
+  for(i=0; i<len; i++){
+    printf("ch[%d] = %u \n", i, ch[i]);
+    if(ch[i] == 255){
+      bit += 8;
+    }else {
+      return bit + next_zero_byte(ch[i]);
+    }
+  }
   return -1;
 }
 
 void print_byte(unsigned char ch){
+  //Printing 
   int i;
   for(i=0; i<8; i++) {
-    int bit = (ch >> i) & 1;
+    int bit = (ch << i) & 1;
     printf("%d", bit );
-
   }
   printf("\n");
-
 }
 
+unsigned char set_bit_of_byte(unsigned char *ch, int len){
+  *ch = *ch | (1 << len);
+  return *ch;
+}
+
+int set_bit_of_bitmap(unsigned char *ch, int len){
+  int byte = len / 8;
+  int bit = len % 8;
+  ch[byte] = set_bit_of_byte(&ch[byte], bit);
+  
+  return 1;
+}
+
+
 int main() {
-  unsigned char ch = 254;
-  printf("print byte\n");
+  unsigned char ch = 1;
   print_byte(ch);
-  printf("first unset bit of char 0 at \n");
-  printf("unset bit at %d \n", scan_byte(ch));
-  
-  //  unsigned char arr[3] = { 255 , 255, 3 };
-  
-  //printf("first unset char at \n");
-  
 
-
+  /*  
+unsigned char arr[3] = { 0 , 0, 0 };
+ 
+  set_bit_at(arr, 5);
+  unset_bit_at(arr, );
+  
+  int i;
+  for(i=0; i<3; i++){
+    printf("arr[%d] = %u \n", i, arr[i]);
+  }
+*/
   return 0;
 }
 
